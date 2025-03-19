8ae08b33738a28a51dde93c609fd718d8236b068
satanTime <m@sudo.eu>
2025-03-19 17:43:16 +0100

    build: supporting simple CI/CD

--- script/src/index.ts
+++ script/src/index.ts
@@ -1,14 +1,14 @@
-import mysql from 'mysql2/promise';
-
-const connection = await mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  charset: process.env.DB_CHARSET,
-  dateStrings: true,
-});
+// import mysql from 'mysql2/promise';
+//
+// const connection = await mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   charset: process.env.DB_CHARSET,
+//   dateStrings: true,
+// });
 
 process.stdout.write(`Hello World\n`);
 
-connection.destroy();
+// connection.destroy();
